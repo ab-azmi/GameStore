cd7641c2ad38f9a3c828a1c674497e620d82d2ab
waniAEbro <82161307+waniAEbro@users.noreply.github.com>
2021-11-28 18:41:15 +0700

    Update functions.php

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -1,3 +1,13 @@
 <?php
-  $connect = mysqli_connect('localhost', 'root', '', 'game_store');
+  $koneksi = mysqli_connect('localhost', 'root', '', 'game_store');
+
+    function query($query){
+        global $koneksi;
+        $result = mysqli_query($koneksi, $query);
+        $rows = [];
+        while($row = mysqli_fetch_assoc($result)){
+            $rows[] = $row;
+        }
+        return $rows;
+    }
 ?>
