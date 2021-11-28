6d75dbab03235815213d1a23894f4e551a0aebf0
waniAEbro <82161307+waniAEbro@users.noreply.github.com>
2021-11-28 18:32:40 +0700

    Update card.php

--- assets/php/card.php
+++ assets/php/card.php
@@ -1,42 +1,23 @@
-<?php require "functions.php" ?>  
+<?php
+    require "functions.php";
+    $kategori = query("SELECT * FROM kategori");
+?>  
+  
 <div class="kategori-title">
-      <h1>Kategori</h1>
-      <a href="#" class="kategori-link">
-          Lainnya
-      </a>
-  </div>
-  <div class="cards-list">
-      <div class="card-custom 1">
-          <div class="card_image"> <img src="https://i.pinimg.com/originals/55/74/03/557403fb6cd94aeb6e6a7407c704b666.gif" /> </div>
-          <div class="card_title title-white">
-              <p>Adventure</p>
-          </div>
-      </div>
-
-      <div class="card-custom 2">
-          <div class="card_image">
-              <img src="https://media2.giphy.com/media/PF5UAWl27jm8ATVsSW/giphy-downsized-large.gif" />
-          </div>
-          <div class="card_title title-white">
-              <p>Racing</p>
-          </div>
-      </div>
-
-      <div class="card-custom 3">
-          <div class="card_image">
-              <img src="https://c.tenor.com/ftDAZSm3nqQAAAAC/lewd-anime.gif" />
-          </div>
-          <div class="card_title title-white">
-              <p>Adult</p>
-          </div>
-      </div>
-
-      <div class="card-custom 4">
-          <div class="card_image">
-              <img src="https://64.media.tumblr.com/93d054deabe68bdb3017cb7e140133e0/b9427c9bde177883-75/s400x600/a3b9e3cb030d82a211e6c80fbe3641a5a926823c.gifv" />
-          </div>
-          <div class="card_title title-white">
-              <p>War</p>
-          </div>
-      </div>
-  </div>
+    <h1>Kategori</h1>
+    <a href="#" class="kategori-link">
+        Lainnya
+    </a>
+</div>
+<div class="cards-list">
+    <?php $i = 1; ?>
+    <?php foreach($kategori as $row) : ?>
+    <div class="card-custom <?php echo $i ?>">
+        <div class="card_image"> <img src="<?php echo $row["gambar"] ?>" /> </div>
+        <div class="card_title title-white">
+            <p><?php echo $row["kategori"] ?></p>
+        </div>
+    </div>
+    <?php $i++; ?>
+    <?php endforeach; ?>
+</div>
