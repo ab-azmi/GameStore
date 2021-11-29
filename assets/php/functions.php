87918f22b14eb4c0aa51c51b4a28c865373d290d
waniAEbro <82161307+waniAEbro@users.noreply.github.com>
2021-11-29 12:55:29 +0700

    Update functions.php

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -1,5 +1,5 @@
 <?php
-  $koneksi = mysqli_connect('localhost', 'root', '', 'game_store');
+    $koneksi = mysqli_connect('localhost', 'root', '', 'game_store');
 
     function query($query){
         global $koneksi;
@@ -9,5 +9,29 @@
             $rows[] = $row;
         }
         return $rows;
+    }
+
+    function insert($data){
+        global $koneksi;
+        $nama_game = htmlspecialchars($data["nama_game"]);
+        $harga = htmlspecialchars($data["harga"]);
+        $tanggal_rilis = htmlspecialchars($data["tanggal_rilis"]);
+        $deskripsi = htmlspecialchars($data["deskripsi"]);
+        $gambar = htmlspecialchars($data["gambar"]);
+        $result = mysqli_query($koneksi, "INSERT INTO games VALUES('', '$nama_game', '$harga', '$deskripsi', '$tanggal_rilis', '$gambar')");
+
+        $id_game = 0;
+        $hasil = mysqli_query($koneksi, "SELECT id_game FROM games ORDER BY id_game DESC LIMIT 1");
+        $row = mysqli_fetch_assoc($hasil);
+        foreach($row as $isi){
+            $id_game = $isi;
+        }
+
+        $kategori = $data["kategori"];
+
+        foreach($kategori as $isi){
+            mysqli_query($koneksi, "INSERT INTO kategori_games VALUES('$id_game', '$isi')");
+        }
+        return mysqli_affected_rows($koneksi);
     }
 ?>
