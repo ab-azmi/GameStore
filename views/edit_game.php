9890f71280606b0f5b27636a00d93680f7facbf4
waniAEbro <82161307+waniAEbro@users.noreply.github.com>
2021-11-30 11:54:16 +0700

    Update edit_game.php

--- views/edit_game.php
+++ views/edit_game.php
@@ -1,25 +1,33 @@
 <?php
 require "../assets/php/functions.php";
+
+$id = $_GET["id"];
+$kategori_lama = query("SELECT
+                        kategori.id_kategori
+                        FROM((kategori_games INNER JOIN games ON kategori_games.id_game = games.id_game) 
+                        INNER JOIN kategori ON kategori_games.id_kategori = kategori.id_kategori) WHERE games.id_game = '$id'");
 $kategori = query("SELECT * FROM kategori");
-$i = 0;
+$game = query("SELECT * FROM games WHERE id_game = '$id'")[0];
+
 if (isset($_POST["submit"])) {
-    if (insert($_POST) > 0) {
+    if (edit($_POST) > 0) {
         echo "
                 <script>
-                    alert('game berhasil ditambahkan');
-                    document.location.href = '../index.php';
+                    alert('game berhasil diubah');
+                    document.location.href = 'admin.php';
                 </script>
             ";
     } else {
         echo "
                 <script>
-                    alert('game gagal ditambahkan');
-                    document.location.href = 'insert.php';
+                    alert('game gagal diubah');
+                    document.location.href = 'edit_game.php';
                 </script>
             ";
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,42 +40,46 @@ if (isset($_POST["submit"])) {
         <div class="container">
             <h1 class="text-center">EDIT GAME</h1>
             <form action="" method="post">
+                <?php $i = 0; foreach($kategori_lama as $row) : ?>
+                    <input type="hidden" name="kategori_lama[]" value="<?php echo $kategori_lama["$i"]["id_kategori"]; ?>">
+                <?php $i++; endforeach; ?>
+                <input type="hidden" name="id_game" value="<?php echo $game["id_game"]; ?>">
                 <div class="row mt-5">
                     <div class="col-6">
                         <div class="webflow-style-input">
-                            <input class="" type="text" name="nama_game" placeholder="Nama Game"></input>
+                            <input class="" type="text" name="nama_game" value="<?php echo $game["name"] ?>"></input>
                             <button type="submit"><i class="fas fa-gamepad"></i></button>
                         </div>
                     </div>
                     <div class="col-6">
                         <div class="webflow-style-input">
-                            <input class="" type="number" name="harga" placeholder="Harga Game"></input>
+                            <input class="" type="number" name="harga" value="<?php echo $game["harga"] ?>"></input>
                             <button type="submit"><i class="fas fa-dollar-sign"></i></button>
                         </div>
                     </div>
                 </div>
                 <div class="col-12 mt-5">
                     <div class="webflow-style-input">
-                        <input type="text" placeholder="Tanggal rilis" name="tanggal_rilis" onfocus="(this.type='date')" onblur="(this.type='text')">
+                        <input type="text" value="<?php echo $game["tanggal_rilis"] ?>" name="tanggal_rilis" onfocus="(this.type='date')" onblur="(this.type='text')">
                         <button type="submit"><i class="far fa-calendar-alt"></i></button>
                     </div>
                 </div>
                 <div class="col-12 mt-5">
                     <div class="webflow-style-input">
-                        <input type="text" placeholder="Deskripsi singkat" name="deskripsi">
+                        <input type="text" value="<?php echo $game["deskripsi"] ?>" name="deskripsi">
                         <button type="submit"><i class="fas fa-quote-left"></i></button>
                     </div>
                 </div>
                 <div class="col-12 mt-5">
                     <div class="webflow-style-input">
-                        <input type="text" placeholder="Url gambar" name="gambar">
+                        <input type="text" value="<?php echo $game["gambar"] ?>" name="gambar">
                         <button type="submit"><i class="far fa-image"></i></button>
                     </div>
                 </div>
 
                 <div class="col-12 mt-5">
                     <div class="webflow-style-input">
-                        <?php foreach ($kategori as $row) : ?>
+                        <?php $i = 0; foreach ($kategori as $row) : ?>
                             <input type="checkbox" name="kategori[]" id="<?php echo $kategori["$i"]["kategori"] ?>" value="<?php echo $kategori[$i]["id_kategori"] ?>">
                             <label for="<?php echo $kategori["$i"]["kategori"] ?>"><?php echo $kategori["$i"]["kategori"] ?></label>
                         <?php $i++;
@@ -75,7 +87,7 @@ if (isset($_POST["submit"])) {
                     </div>
                 </div>
                 <div class="col-3">
-                    <button class="gradient-border mt-5" id="box">
+                    <button class="gradient-border mt-5" id="box" name="submit">
                         Submit
                     </button>
                 </div>
