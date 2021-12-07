a7374cf9bb7abd50d20467942e44b1e9f698cb60
Hanips <75816993+Hanips@users.noreply.github.com>
2021-12-07 22:58:33 +0700

    Update pembayaran.php

--- views/pembayaran.php
+++ views/pembayaran.php
@@ -1,7 +1,7 @@
 <?php
     $conn = mysqli_connect("localhost", "root", "", "game_store");
 
-    $result = mysqli_query($conn, "SELECT * FROM tagihan");
+    $result = mysqli_query($conn, "SELECT * FROM membayar");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,21 +14,27 @@
         <tr>
             <th>No.</th>
             <th>ID Tagihan</th>
-            <th>ID Game</th>
-            <th>Diskon</th>
-            <th>Pajak</th>
-            <th>Total Harga</th>
+            <th>ID User</th>
+            <th>Tanggal Pembayaran</th>
+            <th>Tervalidasi</th>
         </tr>
 
         <?php $i = 1; ?>
         <?php while($row = mysqli_fetch_assoc($result)) : ?>
+        <?php
+        $bool;
+        if ($row["tervalidasi"] > 0){
+            $bool = "Yes";
+        } else {
+            $bool = "No";
+        }
+        ?>
         <tr>
             <td><?= $i; ?></td>
             <td><?= $row["id_tagihan"]; ?></td>
-            <td><?= $row["id_game"]; ?></td>
-            <td><?= $row["diskon"]; ?></td>
-            <td><?= $row["pajak"]; ?></td>
-            <td><?= $row["total_harga"]; ?></td>
+            <td><?= $row["id_user"]; ?></td>
+            <td><?= $row["tanggal_pembayaran"]; ?></td>
+            <td><?= $bool; ?></td>
         </tr>
         <?php $i++; ?>
         <?php endwhile; ?>
