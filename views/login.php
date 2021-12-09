6238691e1532fa83403b1e15b28935bcfb911ffb
waniAEbro <82161307+waniAEbro@users.noreply.github.com>
2021-12-10 06:22:41 +0700

    Update login.php

--- views/login.php
+++ views/login.php
@@ -1,22 +1,9 @@
 <?php
-
-    $conn = mysqli_connect("localhost", "root", "", "game_store");
-    if( isset($_POST["login"]) ) {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        $result = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username'");
-
-        if( mysqli_num_rows($result) === 1 ) {
-            $row = mysqli_fetch_assoc($result);
-            if(password_verify($password, $row["password"])) {
-                header("location: index.php");
-                exit;
-            }
-        }
-    $error = true;
-}
-
+    require "../assets/php/functions.php";
+    session_start();
+    if( isset($_POST["submit"]) ) {
+        $error = login($_POST);
+    }
 ?>
 
 <!DOCTYPE html>
